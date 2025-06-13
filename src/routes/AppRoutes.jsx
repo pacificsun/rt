1bcd56ed82398bbce7666031f416f3bf73fe9669
Suraj Thapa <stsurajst@gmail.com>
2025-06-12 22:40:23 -0500

    bug fix on routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,23 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import Login from '../pages/Login';
-import Home from '../pages/Home';
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+import Home from "../pages/Home";
 
-import ProtectedRoute from '../components/ProtectedRoute';
+import ProtectedRoute from "../components/ProtectedRoute";
 // import NotFound from '../pages/NotFound';
 
 const AppRoutes = () => {
+  return (
     <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-            path="/"
-            element={
-                <ProtectedRoute>
-                    <Home />
-                </ProtectedRoute>
-            }
-             />
-
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
-
-}
+  );
+};
 export default AppRoutes;
